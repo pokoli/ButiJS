bd034a6fb2a567d70814ef318da73ca5301e06a6
pokoli <pokoli@gmail.com>
2012-01-11 21:40:31 +0100

    UI: Fix the chat input to the bottom of the screen.

--- src/public/ui.js
+++ src/public/ui.js
@@ -1,5 +1,13 @@
 $(function() {
+    //Container must fill all the screen  
+    $('#container').height($(window).height()-40);
+    $('#tabs').height($(window).height()-40);
+    //Create the tabs
     $('#tabs').tabs().find( ".ui-tabs-nav" ).sortable({ axis: "x" });
+    var height = $('#tabs').height() -85;
+    alert(height);
+    $('#chat-messages').height(height);
+    $('#chat-players').height(height);
 });
     
 
