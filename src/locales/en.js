f69d1e04baa8f47cc7a02f6f9764d364d4b28f19
pokoli <pokoli@gmail.com>
2012-03-06 19:24:51 +0100

    Added translations

---
+++ src/locales/en.js
@@ -0,0 +1,19 @@
+{
+	"Welcome, who you are?": "Welcome, who you are?",
+	"Not enough players": "Not enough players",
+	"The game is full": "The game is full",
+	"A player can roll only once": "A player can roll only once",
+	"A card can be rolled only once": "A card can be rolled only once",
+	"Card must be from initial suit": "Card must be from initial suit",
+	"Card must be defined": "Card must be defined",
+	"Card must be higher than others team": "Card must be higher than others team",
+	"You can not play a card you don't have in the stack": "You can not play a card you don't have in the stack",
+	"Card must be from thriumph suit": "Card must be from thriumph suit",
+	"Only 4 rolls allowed": "Only 4 rolls allowed",
+	"Game must be an object": "Game must be an object",
+	"You are already in the game": "You are already in the game",
+	"Game %s does not exist": "Game %s does not exist",
+	"No current game running": "No current game running",
+	" %s left the server": " %s left the server",
+	"%s joined the server": "%s joined the server"
+}
