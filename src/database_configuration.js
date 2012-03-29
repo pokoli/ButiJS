42759566aa46643e90db96deae88d1c2f788c5a5
pokoli <pokoli@gmail.com>
2012-03-29 12:26:12 +0200

    Added mongoose and database configuration

---
+++ src/database_configuration.js
@@ -0,0 +1,15 @@
+/*
+    File that hols the database configuration access.
+*/
+
+/*
+   Change the exports to fit your neeeds. 
+   
+   - host: Specifies which server we want to use
+   - database: Specifies which database we whant to use. 
+
+*/
+module.exports = {
+    host: 'localhost',
+    database : 'test',
+}
